bc3929c1835992ea88126ae8f261ced1a35c22ee
Dim Hutsel <dreamtm@yandex.ru>
2018-05-28 20:28:51 +0700

    create project

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+const config = {
+  database: "test",
+  port: 5000,
+  secret: 'secretkey'
+}
+
+export default config;
+
---
+++ src/server.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import session from 'express-session'
+import bodyParser from 'body-parser'
+import morgan from 'morgan'
+import bluebird from 'bluebird'
+
+import config from './config'
+
+const app = express()
+
+mongoose.Promise = bluebird;
+mongoose.connect("mongodb://localhost:27017/" + config.database, err => {
+  if (err) throw err;
+
+  console.log('Mongo connected')
+})
+
+app.listen(config.port, err => {
+  if (err) throw err;
+
+  console.log('Server listening on port: ' + config.port)
+})
+
+
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: config.secret
+}))
+
+app.get('*', async (req, res) => {
+  res.send('Hello World')
+})
